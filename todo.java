e85619c56fa48d4b0b79f4ff83d48f3c3d8303fe
Icedcave <icedcavestich@gmail.com>
2026-04-04 10:23:42 +0200

    Refactor todo function and remove unused variables

--- todo.java
+++ todo.java
@@ -5,7 +5,6 @@ public class todo {
     public static void todoFunction() {
 
         Scanner todoList = new Scanner(System.in); 
-        Scanner createDocs = new Scanner(System.in);
 
         System.out.println("How many Todo tasks you want?");
         int count = todoList.nextInt();
@@ -20,25 +19,12 @@ public class todo {
             String todoWishes = todoList.nextLine();
         }
         System.out.println("Congrats you made your tasks.");
-        String newDocs = createDocs.nextLine();
-        try (FileWriter createFile = new FileWriter("todo.txt")) {
-            // if statement checks if user typed create and it will write data
-            if (newDocs.equals("create")) {
-                createFile.write(count);
-                System.out.println("Your todo list created successfully sir");
-            }    
-        } catch (IOException e) {
-            System.err.println("Sorry couldn't write files.");
-            e.printStackTrace();
-        }
-
         todoList.close();
     }
 
 public static void main(String[] args) {    
         System.out.println("+++++++++++++++++++++");
         System.out.println("Welcome to Todo List!");
-        System.out.println("create = create a text file!");
         todoFunction();
     }
 }
